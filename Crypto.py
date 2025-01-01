b0b763e79f2f9dfb01719992604ce8622efd5591
krarktel112 <krarktel@gmail.com>
2025-01-01 17:52:56 -0500

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -1,7 +1,7 @@
 import requests
 
-def fetch_price(ticker):
-    url = 'https://api.coingecko.com/api/v3/simple/price?ids=' + bitcoin + '&vs_currencies=usd'
+def fetch_price:
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
     response = requests.get(url)
     price_data = response.json()
     return price_data[ticker]['usd']
