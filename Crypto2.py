826fecd0a6f776f2a8ad0c51b82c21ea8927ad22
krarktel112 <krarktel@gmail.com>
2025-03-22 17:17:22 -0400

    Update Crypto2.py

--- Crypto2.py
+++ Crypto2.py
@@ -4,6 +4,7 @@ import time
 portfolio = {
     'BTC': 0,  # Amount of Bitcoin you own
     'ETH': 0.000877,  # Amount of Ethereum you own
+    'ETH': 0.37690818,  # Amount of Ethereum you own
     # Add more cryptocurrencies here
 }
 
