cdf757c9691fb9424da6d00c3d66ca0604041b6f
krarktel112 <krarktel@gmail.com>
2025-03-23 14:26:56 -0400

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -27,6 +27,7 @@ def display_portfolio(portfolio):
         value = amount * price
         total_value += value
         print(f"{ticker}: ${price:.2f} (You own {amount} {ticker}, Value: ${value:.2f})")
+     print(f"{ticker}: ${price:.2f} (You own {amount} {ticker}, Value: ${value:.2f})")
      for ticker, amount in portfolio2.items():
         price = get_crypto_price(ticker)
         value = amount * price
