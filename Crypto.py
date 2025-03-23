d42f85879e739ef7c24c9cb0fc9fb39974e86646
krarktel112 <krarktel@gmail.com>
2025-03-23 14:25:12 -0400

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -6,6 +6,11 @@ portfolio = {
     'ETH': 0.000942,  # Amount of Ethereum you own
     # Add more cryptocurrencies here
 }
+portfolio2 = {
+    #'BTC': 0,  # Amount of Bitcoin you own
+    'ETH': 0.000942,  # Amount of Ethereum you own
+    # Add more cryptocurrencies here
+}
 
 # Note some tokens aren't available on Coinbase
 def get_crypto_price(ticker):
@@ -22,12 +27,11 @@ def display_portfolio(portfolio):
         value = amount * price
         total_value += value
         print(f"{ticker}: ${price:.2f} (You own {amount} {ticker}, Value: ${value:.2f})")
-     for ticker, amount in portfolio.items():
+     for ticker, amount in portfolio2.items():
         price = get_crypto_price(ticker)
         value = amount * price
         total_value += value
-        print(f"{ticker}: ${price:.2f} (You own {amount} {ticker}, Value: ${value:.2f})")
-    print(f"Total Portfolio Value: ${total_value:.2f}\n")
+    print(f"Total Reward Value: ${total_value:.2f}\n")
 
 def main():
     while True:
