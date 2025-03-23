87ccb1bdb92199688cd6730f4b057b71abfa9de4
krarktel112 <krarktel@gmail.com>
2025-03-23 14:40:20 -0400

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -28,12 +28,7 @@ def display_portfolio(portfolio):
         total_value += value
         print(f"{ticker}: ${price:.2f} (You own {amount} {ticker}, Value: ${value:.2f})")
      print(f"Total Portfolio Value: ${total_value:.2f}\n")
-     total_value = 0.0
-     for ticker, amount in portfolio2.items():
-        price = get_crypto_price(ticker)
-        value = amount * price
-        total_value += value
-     print(f"Total Reward Value: ${total_value:.2f}\n")
+     
 
 def main():
     while True:
