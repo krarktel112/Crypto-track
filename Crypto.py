ef68560d5cdb4b9ec8a52b8329cfc4631a180b0a
krarktel112 <krarktel@gmail.com>
2025-03-24 12:51:44 -0400

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -36,7 +36,7 @@ def main():
         display_portfolio(portfolio)
         display_portfolio(portfolio2)
         time.sleep(30)
-        clear()
+        os.system('clear')
 
 if __name__ == "__main__":
     main()
