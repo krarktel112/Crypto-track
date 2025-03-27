e0df54287987e86dad2fb252bfc7d1e2896fc789
krarktel112 <krarktel@gmail.com>
2025-03-27 15:50:37 -0400

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -4,13 +4,13 @@ import os
 
 portfolio = {
     'BTC': 0,  # Amount of Bitcoin you own
-    'ETH': 0.37792549,  # Amount of Ethereum you own
+    'ETH': 0.37797165,  # Amount of Ethereum you own
     # Add more cryptocurrencies here
 }
 
 portfolio2 = {
     'BTC': 0,  # Amount of Bitcoin you own
-    'ETH': 0.001017,  # Amount of Ethereum you own
+    'ETH': 0.001063,  # Amount of Ethereum you own
     # Add more cryptocurrencies here
 }
 # Note some tokens aren't available on Coinbase
