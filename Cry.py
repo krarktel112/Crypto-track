04f97b89cbc13f4c35350dc58eb35b057da89eb8
krarktel112 <krarktel@gmail.com>
2025-05-11 15:25:20 -0400

    Update Cry.py

--- Cry.py
+++ Cry.py
@@ -11,7 +11,7 @@ profit1 = round(((x*0.00008288)-8), 2)
 y = fetch_price("ethereum")
 current_value2 = round((y*0.37887184), 2)
 current_value3 = round((y*0.001963), 2)
-profit2 = (current_value2-1092.2)
+profit2 = round(((y*0.37887184)-1092.2), 2)
 profit3 = current_value3
 print(f"Bitcoin: {x} | Current value: {current_value1} | Profit: {profit1}")
 print(f"Ethereum: {y} | Current value: {current_value2} | Profit: {profit2}")
