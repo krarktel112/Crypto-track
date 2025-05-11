9698ce94a51f66495a12c9ee3bf9b2e642db5eb0
krarktel112 <krarktel@gmail.com>
2025-05-11 14:14:00 -0400

    Create Cry.py

---
+++ Cry.py
@@ -0,0 +1,7 @@
+import requests
+
+def fetch_price(ticker):
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=' + ticker + '&vs_currencies=usd'
+    response = requests.get(url)
+    price_data = response.json()
+    return price_data[ticker]['usd']
