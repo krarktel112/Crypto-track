35a71855624d47fb32307e5b793bedb4db6389ea
krarktel112 <krarktel@gmail.com>
2025-05-11 15:21:26 -0400

    Update Cry.py

--- Cry.py
+++ Cry.py
@@ -7,7 +7,7 @@ def fetch_price(ticker):
     return price_data[ticker]['usd']
 x = fetch_price("bitcoin")
 current_value1 = round((x*0.00008288), 2)
-profit1 = round((x*0.00008288)-8)), 2)
+profit1 = round(((x*0.00008288)-8), 2)
 y = fetch_price("ethereum")
 current_value2 = round((y*0.37887184), ndigits)
 current_value3 = round((y*0.001963), 2)
