f65ea72e4ee95a9841f995763c5b19a3b8644c0f
krarktel112 <krarktel@gmail.com>
2025-05-15 14:24:03 -0400

    Update Cry.py

--- Cry.py
+++ Cry.py
@@ -43,7 +43,7 @@ def display_portfolio(portfolio, rewards):
         price = int(fetch_price(ticker))
         price1 = round(price, 2)
         value = amount * int(price)
-        total_value += value
+        total_value += float(value)
         value1 = round(value, 2)
         #total_value1 = round(total_value, 2)
         print(f"{ticker}: ${price1} (You own {amount} {ticker}, Value: ${value1})")
