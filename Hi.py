805a9ecea56c9e1ee5a30396c2d28ac1b4b1ac6a
krarktel112 <krarktel@gmail.com>
2025-06-15 16:02:49 -0400

    Update Hi.py

--- Hi.py
+++ Hi.py
@@ -1,1 +1,6 @@
-https://30rates.com/ethereum-price-prediction-tomorrow-week-month-eth-forecast
+import requests, time, os, BeautifulSoup 
+
+url = 'https://30rates.com/ethereum-price-prediction-tomorrow-week-month-eth-forecast'
+response = requests.get(url)
+data = response.json()
+print(data)
