0edbe6a1e553e84ee8f917fc3ac79e0372d9d4b2
krarktel112 <krarktel@gmail.com>
2025-06-15 16:09:48 -0400

    Update Hi.py

--- Hi.py
+++ Hi.py
@@ -2,5 +2,4 @@ import requests, time, os
 
 url = 'https://30rates.com/ethereum-price-prediction-tomorrow-week-month-eth-forecast'
 response = requests.get(url)
-data = response.json()
-print(data)
+print(response)
