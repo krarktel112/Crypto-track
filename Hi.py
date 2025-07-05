8be48067125070cad48fe7d61295896b8f9a6315
krarktel112 <krarktel@gmail.com>
2025-07-05 12:33:08 -0400

    Delete Hi.py

--- Hi.py
+++
@@ -1,9 +0,0 @@
-import requests, time, os
-from bs4 import BeautifulSoup
-
-url = 'https://30rates.com/ethereum-price-prediction-tomorrow-week-month-eth-forecast'
-response = requests.get(url)
-html_content = response.text
-#soup = 
-
-print(html_content)
