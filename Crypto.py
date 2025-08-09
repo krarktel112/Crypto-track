67754db9af4126efc7855ede39d715729b5008f6
krarktel112 <krarktel@gmail.com>
2025-08-09 12:16:29 -0400

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -2,14 +2,14 @@ import requests, time, os
 
 portfolio = {
     'BTC': 0.00150331,  # Amount of Bitcoin you own
-    'ETH': 0.0027148001,  # Amount of Ethereum you own
+    'ETH': 0.00271517,  # Amount of Ethereum you own
     'SOL': 0.001037659
     # Add more cryptocurrencies here
 }
 
 portfolio2 = {
     #'BTC': 0,  # Amount of Bitcoin you own
-    'ETH': 0.0000836201,  # Amount of Ethereum you own
+    'ETH': 0.0000839961,  # Amount of Ethereum you own
     'SOL': 0.001037659
     # Add more cryptocurrencies here
 }
