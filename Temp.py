70ba7859426d4840b3a55b2d94c9a9f218c1c554
krarktel112 <krarktel@gmail.com>
2025-08-14 11:47:38 -0400

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -5,11 +5,11 @@ bitcoin_price_at_purchase = input("Price:") # Bitcoin price at time of purchase
 
 
     # Calculate the amount of Bitcoin purchased
-bitcoin_purchased = (initial_investment * (1 - buy_fee_percentage)) / bitcoin_price_at_purchase
-total_cost = initial_investment + (initial_investment * buy_fee_percentage) 
+bitcoin_purchased = (initial_investment * 0.98)) / bitcoin_price_at_purchase
+total_cost = initial_investment + (initial_investment * 0.02) 
 
     # Calculate the break-even selling price (accounting for the sell fee as a percentage of the selling price)
-break_even_price = total_cost / (bitcoin_purchased * (1 - sell_fee_percentage))
+break_even_price = total_cost / (bitcoin_purchased * (0.98))
 
 
 # Example Usage:
