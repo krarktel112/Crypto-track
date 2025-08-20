63486ba72e17208daf2fd0b7c1d79121c42a1e8a
krarktel112 <krarktel@gmail.com>
2025-08-20 19:03:18 -0400

    Update and rename Cashapp to Cashapp.py

---
+++ Cashapp.py
@@ -0,0 +1,34 @@
+# Using 'and'
+value = 7
+lower_bound = 5
+upper_bound = 10
+if value >= lower_bound and value <= upper_bound:
+    print(f"{value} is between {lower_bound} and {upper_bound} (inclusive).")
+
+# Using chained comparison
+if lower_bound <= value <= upper_bound:
+    print(f"{value} is between {lower_bound} and {upper_bound} (inclusive).")
+$0 - $9.99
+3%
+
+$10 - $100
+2.25%
+
+$100.01 - $200
+2%
+
+$200.01 - $1000
+
+1.75%
+
+$1000.01 - $2000 
+1.5%
+
+$2000.01 - $3000
+1.25%
+
+$3000.01 - $5000
+1%
+
+>$5,000.01
+0.75%
