697636184c6ef66e83d4980b66455b7245fb1b82
krarktel112 <krarktel@gmail.com>
2025-08-20 21:43:28 -0400

    Update Break.py

--- Break.py
+++ Break.py
@@ -35,7 +35,7 @@ def calculate_bitcoin_break_even(initial_investment, buy_fee_percentage, sell_fe
 initial_investment = input("Investment:") # $1000 USD
 buy_fee_percentage = 0.02  # 0.1% buy fee
 sell_fee_percentage = 0.02  # 0.1% sell fee
-bitcoin_price_at_purchase = input("Price:") # Bitcoin price at time of purchase
+bitcoin_price_at_purchase = get_crypto_price(BTC) # Bitcoin price at time of purchase
 
 break_even = calculate_bitcoin_break_even(initial_investment, buy_fee_percentage, sell_fee_percentage, bitcoin_price_at_purchase)
 break_even1 = calculate_bitcoin_break_even(initial_investment, 0.03, 0.03, bitcoin_price_at_purchase)
