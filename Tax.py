8ff20c4a6f13374810ad647bdc9f4fe3e7c59b97
krarktel112 <krarktel@gmail.com>
2025-09-28 12:25:41 -0400

    Update Tax.py

--- Tax.py
+++ Tax.py
@@ -1,4 +1,16 @@
-federal 10
-state 3.05
-local 1.59
-medical 64$
+rate = input("Rate: ") or 0
+ot = rate*1.5
+hours = input("Hours: ") or 0
+minutes = input("Minutes: ") or 0
+minutes = minutes / 60
+hours = hours + minutes 
+if hours <= 40:
+  gross = hours * rate
+elif hours > 40:
+  gross1 = 40 * rate
+  gross2 = (hours-40)*ot
+  gross = gross1 + gross2
+else:
+  print("Error")
+
+print(gross-49.84-((gross-49.84)*(0.084+0.03+0.0159+0.0145+0.062))-13.75)
