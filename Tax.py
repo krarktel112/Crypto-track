6098a12b2c873f3101664b8ba814df6ec5c009c9
krarktel112 <krarktel@gmail.com>
2025-09-28 12:35:16 -0400

    Update Tax.py

--- Tax.py
+++ Tax.py
@@ -3,14 +3,14 @@ ot = int(rate)*1.5
 hours = input("Hours: ") or 0
 minutes = input("Minutes: ") or 0
 minutes = int(minutes) / 60
-hours = hours + minutes 
+hours = int(hours) + int(minutes)
 if hours <= 40:
-  gross = hours * rate
+  gross = int(hours) * int(rate)
 elif hours > 40:
-  gross1 = 40 * rate
-  gross2 = (hours-40)*ot
-  gross = gross1 + gross2
+  gross1 = 40 * int(rate)
+  gross2 = (int(hours)-40)*int(ot)
+  gross = int(gross1) + int(gross2)
 else:
   print("Error")
 
-print(gross-49.84-((gross-49.84)*(0.084+0.03+0.0159+0.0145+0.062))-13.75)
+print(int(gross)-49.84-((int(gross)-49.84)*(0.084+0.03+0.0159+0.0145+0.062))-13.75)
