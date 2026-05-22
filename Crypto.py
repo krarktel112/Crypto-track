1b48988b7682cea07c9a4fd534980ab328a34573
krarktel112 <krarktel@gmail.com>
2026-05-22 10:42:03 -0400

    Update SOL amounts in portfolio dictionaries

--- Crypto.py
+++ Crypto.py
@@ -3,7 +3,7 @@ import requests, time, os
 portfolio = {
     #'BTC': 0,  # Amount of Bitcoin you own
     'ETH': 0.00000364,  # Amount of Ethereum you own
-    'SOL': 0.051702936
+    'SOL': 0.051756079
     # Add more cryptocurrencies here
 }
 
@@ -12,7 +12,7 @@ portfolio2 = {
     #0.054997936 sol
     #'BTC': 0.000066,  # Amount of Bitcoin you own
     'ETH': 0.00000364,  # Amount of Ethereum you own
-    'SOL': 0.000277492
+    'SOL': 0.000298392
     # Add more cryptocurrencies here
 }
 # Note some tokens aren't available on Coinbase
