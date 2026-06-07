ebf931b47d5d418b7d52598a2360b97a00ba755d
krarktel112 <krarktel@gmail.com>
2026-06-07 15:09:23 -0400

    Update conversion function for recovery values
    Refactor recovery calculations and add new value computations.

--- Crypto.py
+++ Crypto.py
@@ -60,16 +60,22 @@ def conversion():
     bitcoin = get_crypto_price("BTC")
     ethereum = get_crypto_price("ETH")
     solana = get_crypto_price("SOL")
-    recover = (10216.3-600-4000)
-    recover2 = (
+    recover = 10216.3
+    recover2 = recover-((4600+1469.97+496.28)*2)
     value = (((recover/ethereum)/0.04307645)*0.15)
     value2 = (((recover/solana)/0.051702936)*0.01)
+    value3 = (((recover2/ethereum)/0.04307645)*0.15)
+    value4 = (((recover2/solana)/0.051702936)*0.01)
     x = ethereum/recover
     y = recover/solana
     rounded_num = round(value, 2)
     rounded_num2 = round(value2, 2)
     rounded_num3 = round(x, 5)
     rounded_num4 = round(y, 5)
+    rounded_num6 = round(value, 2)
+    rounded_num7 = round(value2, 2)
+    rounded_num8 = round(x, 5)
+    rounded_num9 = round(y, 5)
     print(f"Ethereum {rounded_num3}: ${value:.2f}")
     print(f"Solana {rounded_num4}: ${value2:.2f}")
 
